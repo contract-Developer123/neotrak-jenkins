58c299714a0baa9cd222c42e326e07f3218c9bdb
contract-Developer123 <developercontract123@gmail.com>
2025-09-15 22:11:53 +0530

    exe

--- config-scanner.js
+++ config-scanner.js
@@ -1,4 +1,4 @@
-const { exec, spawn } = require('child_process');
+const { exec, execSync, spawn } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
